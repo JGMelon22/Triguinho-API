44b87884a9621b93f01abdc8ef52698c008b824f
JGMelon22 <jgvasconcellos22@gmail.com>
2025-09-20 14:57:09 -0300

    feat: develop Pagination Offset base class

---
+++ src/Triguinho.Core/Shared/PagedResponseOffset.cs
@@ -0,0 +1,18 @@
+﻿namespace Triguinho.Core.Shared;
+public record class PagedResponseOffset<T>
+{
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+    public int TotalRecords { get; init; }
+    public int TotalPages { get; init; }
+    public List<T> Data { get; init; }
+
+    public PagedResponseOffset(int pageNumber, int pageSize, int totalRecords, int totalPages, List<T> data)
+    {
+        Data = data;
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalRecords = totalRecords;
+        TotalPages = (int)Math.Ceiling((decimal)totalRecords / (decimal)pageSize);
+    }
+}
