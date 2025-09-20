3808e7ca1441596792a1af988a33d963cd4c8571
JGMelon22 <jgvasconcellos22@gmail.com>
2025-09-20 14:44:17 -0300

    feat: develop Result T pattern class

---
+++ src/Triguinho.Core/Shared/Error.cs
@@ -0,0 +1,13 @@
+﻿namespace Triguinho.Core.Shared;
+public sealed record Error(int Code, string Description)
+{
+    // Bet-Related Errors
+    public static Error BetNotFound=> new Error(404, "Bet Id not found!");
+
+    // Game-Related Errors
+    public static Error GameNotFound => new Error(404, "Game Id not found!");
+
+    // Round-Related Errors
+    public static Error RoundNotFound => new Error(404, "Round Id not found!");
+
+}
---
+++ src/Triguinho.Core/Shared/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace Triguinho.Core.Shared;
+public sealed class Result<T>
+{
+    public T? Value { get; }
+    public Error? Error { get; }
+    public bool IsSuccess { get; }
+    public bool IsError => !IsSuccess;
+
+    public Result(T? value)
+    {
+        Value = Value ?? throw new ArgumentException(nameof(value), "Value cannot be null.");
+        IsSuccess = true;
+        Error = null;
+    }
+
+    public Result(Error error)
+    {
+        Value = Value ?? throw new ArgumentException(nameof(error), "Error cannot be null.");
+        IsSuccess = false;
+        Error = error;
+    }
+
+    public static Result<T> Success(T value) => new(value);
+    public static Result<T> Failure(Error error) => new Result<T>(error);
+}
